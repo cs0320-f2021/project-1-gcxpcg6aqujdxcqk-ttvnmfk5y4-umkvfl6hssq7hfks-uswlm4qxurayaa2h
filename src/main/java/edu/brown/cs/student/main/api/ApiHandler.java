8dfa2e463ad380fdbddce70a1e7df77defe98fda
gcxpcg6aqujdxcqk <gcxpcg6aqujdxcqk@gmail.com>
2021-10-17 00:43:54 +0000

    comments on ApiHandler

--- src/main/java/edu/brown/cs/student/main/api/ApiHandler.java
+++ src/main/java/edu/brown/cs/student/main/api/ApiHandler.java
@@ -6,7 +6,6 @@ import edu.brown.cs.student.main.jsonobjects.JSONObject;
 import edu.brown.cs.student.main.jsonobjects.JsonHandler;
 import edu.brown.cs.student.main.jsonobjects.Reviews;
 import edu.brown.cs.student.main.jsonobjects.Users;
-
 import java.util.Arrays;
 import java.util.HashSet;
 
@@ -18,8 +17,10 @@ public class ApiHandler implements ArgumentHandler {
   private HashSet<JSONObject> hashSet;
 
   /**
+   * This method makes requests to each endpoint and compiles the received data, storing the
+   * result in a hashset.
    *
-   * @param arguments
+   * @param arguments to determine user, rent, or reviews
    */
   public void handleArg(String[] arguments) {
     ApiClient client = new ApiClient();
@@ -51,7 +52,6 @@ public class ApiHandler implements ArgumentHandler {
 
         System.out.println(userList.size() + " results!");
         hashSet = userList;
-
         break;
       case "reviews":
         HashSet<JSONObject> reviewList = new HashSet<>();
@@ -78,7 +78,6 @@ public class ApiHandler implements ArgumentHandler {
 
         System.out.println(reviewList.size() + " results!");
         hashSet = reviewList;
-
         break;
       case "rent":
         HashSet<JSONObject> rent = new HashSet<>();
@@ -114,16 +113,33 @@ public class ApiHandler implements ArgumentHandler {
     }
   }
 
+  /**
+   * This method checks the number of passed in arguments, and returns true if it equals the
+   * expected number. Method used to ensure correct REPL commands.
+   *
+   * @param arguments passed into the REPL
+   * @return true if arguments equal the expected number
+   */
   public boolean checkNumArgs(String[] arguments) {
     return arguments.length == 2;
   }
 
+  /**
+   * This method returns the usage string providing the user more information on how to properly
+   * use the command.
+   *
+   * @return usage string
+   */
   public String getUsageString() {
-    return "usage: somethingGet <what>";
+    return "usage: dataGet <data_type>";
   }
 
+  /**
+   * This method returns the hashset with the collected data.
+   *
+   * @return hashset
+   */
   public HashSet<JSONObject> getHashSet() {
     return hashSet;
   }
-
 }
